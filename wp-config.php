e96ceff5fd0b5bbb2ac874a004d34c0a9e7d00ae
jchck <hey@justinchick.com>
2016-07-14 15:03:54 -0400

    load wp

--- wp-config.php
+++ wp-config.php
@@ -56,4 +56,4 @@ if ( ! defined( 'ABSPATH' )) {
 // ==============================================================
 // Bootstrap WordPress
 // ==============================================================
-require_once( 'ABSPATH' . 'wp-settings.php');
+require_once( ABSPATH . 'wp-settings.php');
