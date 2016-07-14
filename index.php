61c20f58976af1f4e2f74be35d4a880169463e7f
jchck <hey@justinchick.com>
2016-07-14 14:17:21 -0400

    Hello, world that shizz

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
 /* Boostrap Wordpress */
-define( 'WP_USE_THEMES', true );
+// define( 'WP_USE_THEMES', true );
 
-require( __DIR__ . '/wp/wp-blog-header.php' );
+// require( __DIR__ . '/wp/wp-blog-header.php' );
+
+?>
+
+<h1>Hello world!</h1>
